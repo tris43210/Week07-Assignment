a7c6039e220bb37e2ffacdcc2487f7bfc5475197
tris43210 <tristan.g.king@gmail.com>
2025-10-16 15:26:44 +0100

    basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        THIS IS THE DEFAULT APP
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import pg from "pg";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+dotenv.config(); 
+
+app.get(`/`, async function (req, res) {
+  res.json({ message: "this is the root route" });
+});
+
+app.listen(`8080`, function () {
+  console.log(`server running on port: http://localhost:8080`);
+});
+
+
